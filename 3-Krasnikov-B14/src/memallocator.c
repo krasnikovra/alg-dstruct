95ba3c9c5f3c2db5a28edfe24ebadd9a7f62d8d9
krasnikovra <roma_krasnikov02@mail.ru>
2021-09-24 13:55:43 +0300

    Anti-fragmentation: freed block is being merged with right-side free block

--- 3-Krasnikov-B14/src/memallocator.c
+++ 3-Krasnikov-B14/src/memallocator.c
@@ -30,18 +30,21 @@ void memdone() {
     desc_t* iter = (desc_t*)s_pmemory;
     while ((char*)iter < (char*)s_pmemory + s_size) {
         if (iter->size < 0)
-            printf("[MEMORY LEAK] Block at 0x%p\n", iter + 1); // info for user so ptr not on desc but on user's part of block
+            // info for user so ptr not on desc but on user's part of block
+            printf("[MEMORY LEAK] block at 0x%p\n", iter + 1); 
         iter = (desc_t*)((char*)iter + _myabs(iter->size));
     }
 }
 
 void* memalloc(int size) {
+    if (size < 1)
+        return NULL;
     // searching for the best fit block
     int block_found = 0;
     desc_t* iter_prev = NULL;
     desc_t* iter = s_head;
     desc_t* bestfit = iter; // descriptor of best fit block
-    desc_t* bestfit_prev = iter_prev;
+    desc_t* bestfit_prev = NULL;
     while (iter) {
         if (iter->size >= memgetblocksize() + size) {
             block_found = 1;
@@ -54,7 +57,7 @@ void* memalloc(int size) {
         iter = iter->next;
     }
     if (!block_found)
-        return 0;
+        return NULL;
     // if we have memory to set new descriptor
     if (bestfit->size >= memgetblocksize() + size + memgetblocksize()) {
         // filling new descriptor for left part of block
@@ -76,7 +79,7 @@ void* memalloc(int size) {
     }
     bestfit->size = -bestfit->size; // if block is allocated for user its size is negative
     bestfit->next = NULL;
-    // returning ptr on memory after bestfit descriptor
+    // returning ptr on memory after bestfit descriptor for user usage
     return (void*)(bestfit + 1);
 }
 
@@ -85,6 +88,24 @@ void memfree(void* p) {
     pdesc->size = -pdesc->size; // converting size to positive which means block is not allocated for user now
     pdesc->next = s_head;
     s_head = pdesc;
+    // anti-fragmentation feauture: if we have free block in the right of freed we need to merge them in one block
+    desc_t* right_block_desc = (desc_t*)((char*)pdesc + pdesc->size);
+    // if we have any block to the right side
+    if ((char*)right_block_desc < (char*)s_pmemory + s_size)
+        //and this block is free
+        if (right_block_desc->size > 0) {
+            // search to the right_block_desc_prev in free space list
+            desc_t* right_block_desc_prev = s_head;
+            desc_t* iter = s_head;
+            while (iter)
+                if (iter->next == right_block_desc) {
+                    right_block_desc_prev = iter;
+                    break;
+                }
+            // skipping right_block_desc in free space list as it will be merged with pdesc
+            right_block_desc_prev->next = right_block_desc->next;
+            pdesc->size += right_block_desc->size;
+        }
 }
 
 int memgetminimumsize() {
